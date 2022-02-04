6db513d3032a6cfc164a149c7deefb5333b09b9d
Joel VG <cgjavg@gmail.com>
2022-02-04 11:50:03 -0400

    New components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Task } from './components/Task/task';
+import TestClassTODO, {TestFunction} from './components/test';
+
+const DATA = [
+  {name: 'Tarea 1', completed: true, steps: []},
+  {name: 'Tarea 2', completed: false, steps: [{name: 'step 1', completed: false}]},
+  {name: 'Tarea 3', completed: false, steps: [{name: 'step 1', completed: false}, {name: 'step 2', completed: false}]}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <TestClassTODO nombre={'Juan'} />
+      <TestClassTODO nombre={'Pepe'} />
+      <TestClassTODO nombre={'Ramon'} />
+      <TestFunction nombre={'Ramiro'}/>
+      <div>
+        Hola  esta es la primera APP, como esta?
+        {DATA.map((task) => <Task item={task}/>)}
+      </div>
     </div>
   );
 }
